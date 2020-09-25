b6ef27f124525879b628e5a749aa69895e9fb1ed
Juan Mireles <jmireles12@gmail.com>
2020-09-25 07:37:30 -0500

    playing with danger

---
+++ src/demonymapp/demonym.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Demonym(props) {
+    return (
+        <div className='demonym'>
+            A {props.name} comes from {props.country}
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './playingwithdanger/App';
 import './index.css';
-import HelloWorld from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
